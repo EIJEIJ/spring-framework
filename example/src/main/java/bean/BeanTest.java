2e4a240815983824cdc82a429cb0470d0a1fc418
zhaojie <zhaojie@ideamake.cn>
2021-10-13 15:52:11 +0800

    增加自学分析模块

---
+++ example/src/main/java/bean/BeanTest.java
@@ -0,0 +1,8 @@
+package bean;
+
+/**
+ * @author zhaojie
+ * @date 2021/10/13 15:51:22
+ */
+public class BeanTest {
+}
---
+++ example/src/main/java/core/ApplicationTest.java
@@ -0,0 +1,8 @@
+package core;
+
+/**
+ * @author zhaojie
+ * @date 2021/10/13 15:51:34
+ */
+public class ApplicationTest {
+}
