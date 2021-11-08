564b2705025d132ed5d16d4ee3cfdc4333d2081c
zhaojie <1790589060@qq.com>
2021-11-08 15:18:33 +0800

    新增 Spring 整合 Mybatis

--- example/src/main/java/cn/analysis/Application.java
+++ example/src/main/java/cn/analysis/Application.java
@@ -1,5 +1,6 @@
 package cn.analysis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 @SpringBootApplication
 @EnableTransactionManagement
+// @MapperScan("cn.analysis.mapper")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ example/src/main/java/cn/analysis/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package cn.analysis.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zhaojie
+ * @date 2021/11/05 10:00:58
+ */
+public interface TestMapper {
+}
